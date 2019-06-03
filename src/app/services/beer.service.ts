900ec7c2fb40c7947b523fe180dc8bb24227994d
Abdul Rahman Hamidy <arahmancsd@live.com>
2019-06-03 10:31:11 +0100

    navbar changed to primary color

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -10,16 +10,17 @@ import { Result, BeerResult } from '../model/beer.model';
 })
 export class BeerService {
   rootUrl:string = "https://thingproxy.freeboard.io/fetch/https://sandbox-api.brewerydb.com/v2/";
+  private url:string;
   constructor(private httpClient: HttpClient) {
   }
   public getBeers(path: string, params: HttpParams, apiKey: string): Observable<Result> {
-    const url = this.rootUrl + path.replace('/','') + '/?key=' + apiKey;
-    return this.httpClient.get<Result>(url, { params, responseType: 'json' })
+    this.url = this.rootUrl + path.replace('/','') + '/?key=' + apiKey;
+    return this.httpClient.get<Result>(this.url, { params, responseType: 'json' })
       .pipe(catchError(this.handleHttpClientError));
   }
   public getBeer(path: string, id: string, apiKey: string): Observable<BeerResult> {
-    const url = this.rootUrl + path.replace('/','') + '/' + id + '/?key=' + apiKey;
-    return this.httpClient.get<BeerResult>(url, { responseType: 'json' })
+    this.url = this.rootUrl + path.replace('/','') + '/' + id + '/?key=' + apiKey;
+    return this.httpClient.get<BeerResult>(this.url, { responseType: 'json' })
       .pipe(catchError(this.handleHttpClientError));
   }
   private handleHttpClientError(errorResponse: HttpErrorResponse) {
